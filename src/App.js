91bd0c0d986aa89e2af4bbe9a7b57c8aebab6073
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-01-26 20:02:05 +0600

    issue solved user.email is working hhahaha

--- src/App.js
+++ src/App.js
@@ -7,24 +7,35 @@ function App() {
 
   //Here , i am use two hook . One for fake data and one for selected friend
   const [users , setUsers] = useState([...peoples]);
-  const [friend , setFriend] = useState([]);
+  const [friends , setFriends] = useState([]);
 
   //Here , this arrow function add user in friend hooks.
   const addUserHandler = (user)=>{
+     
+               const searchFriend = friends.find(friend=> user.Email=== friend.Email );
+              // console.log(searchFriend);
+               
+               if(searchFriend === undefined){
 
-                const newFriend = [...friend, user];
-                setFriend(newFriend);
+                       const newFriend = [...friends, user];
+                       setFriends(newFriend);
+
+               }
+               else
+               {
+                    alert('This friend already added');
+               }
 }
 
 //Use reduce method for totol income in friend list
-  const total = friend.reduce((sum , user)=> sum + user.Income , 0);
+  const total = friends.reduce((sum , user)=> sum + user.Income , 0);
 
   return (
     <div >
            <div style = {{textAlign: 'center'}}>
                       <h3>Total User : {users.length}</h3>
-                     <h4>Add Friend : {friend.length}</h4>
-                    <p>Friend Income : ${total} </p>
+                      <h4>Add Friend : {friends.length}</h4>
+                      <p>Friend Income : ${total} </p>
            </div>
 
             {
